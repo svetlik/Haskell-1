d99eeb38ec7a088669ecd168b34e4319387c7d4f
svetlozaurus <svetlik@gmail.com>
2015-06-07 12:32:48 +0300

    Add solution for 02-LanguageCame

---
+++ Application/02-LanguageCame/solution.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+class LanguageGame
+
+  attr_reader :language, :word
+
+  def initialize(language, word)
+    @language = language
+    @word = word
+  end
+
+  def check
+    validate_letters && validate_length ? 'yes' : 'no'
+  end
+
+  def validate_letters
+    @word == valid_word
+  end
+
+  def validate_length
+    valid_word.length == valid_word_length
+  end
+
+  def valid_word
+    valid_word = ""
+    unique_letters.each {|letter| valid_word += letter * sequence_length}
+    valid_word
+  end
+
+  def valid_word_length
+    base.length * sequence_length
+  end
+
+  def base
+    @language.gsub("^n", '')
+  end
+
+  def unique_letters
+    unique_letters = []
+    @word.each_char {|char| unique_letters << char}
+    unique_letters.uniq!
+  end
+
+  def first_letter
+    @word.chr
+  end
+
+  def sequence_length
+    @word.rindex(first_letter) + 1
+  end
+end
+
+print "Define a lanugage? "
+language = gets.chomp
+print "Give a word? "
+word = gets.chomp
+
+puts game_result = LanguageGame.new(language, word).check
