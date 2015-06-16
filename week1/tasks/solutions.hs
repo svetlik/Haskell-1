656c3477431e60afe68b212c771cc35e452564ac
svetlozaurus <svetlik@gmail.com>
2015-06-17 00:30:49 +0300

    Add solutions for tasks 1-7

---
+++ week1/tasks/solutions.hs
@@ -0,0 +1,13 @@
+even' x = mod x 2 == 0
+
+odd' x = mod x 2 /= 0
+
+bmi height weight = weight / (height^2)
+
+deg2Rad degrees = pi / 180 * degrees
+
+rad2Deg radians = 180 / pi * radians
+
+isTriangle a b c = a + b > c && a + c > b && b + c > a
+
+perimeter a b c = a + b + c
