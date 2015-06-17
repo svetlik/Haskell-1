9535072be2e2095f98d6f7eabe30ac43e299c29f
svetlozaurus <svetlik@gmail.com>
2015-06-18 02:29:27 +0300

    Add solutions for tasks 8-10 (wip: 10)

--- week1/tasks/solutions.hs
+++ week1/tasks/solutions.hs
@@ -11,3 +11,17 @@ rad2Deg radians = 180 / pi * radians
 isTriangle a b c = a + b > c && a + c > b && b + c > a
 
 perimeter a b c = a + b + c
+
+perimeterHalf a b c = (perimeter a b c) / 2
+
+area a b c = sqrt((perimeterHalf a b c) * ((perimeterHalf a b c) - a) * ((perimeterHalf a b c) - b) * ((perimeterHalf a b c) - c))
+
+calculate sign a b = if sign == '+' then a + b else if sign == '-' then a - b else a * b
+
+convert fromCurrency toCurrency amount = if fromCurrency == 'usd' then if toCurrency == 'bgn' then amount * 1.737
+                                                                  else if toCurrency == 'eur' then amount * 1.959 / 1.737
+                                         else if fromCurrency == 'eur' then if toCurrency == 'bgn' then amount * 1.959
+                                                                       else if toCurrency == 'usd' then amount * 1.737 / 1.959
+                                         else if fromCurrency == 'bgn' then if toCurrency == 'usd' then amount / 1.737
+                                                                       else if toCurrency == 'eur' then amount / 1.959
+                                                                        
